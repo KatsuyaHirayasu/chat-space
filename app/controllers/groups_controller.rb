6ee2fd34486fb44f2ed2b0ffd4693a6d952b2c00
KatsuyaHirayasu <o237066k@mail.kyutech.jp>
2017-06-02 17:20:04 +0900

    edited some wrong files

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,41 +1,44 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:index, :edit, :update]
+  before_action :set_group, only: [:show, :edit, :update]
 
-  def index
-    @groups = current_user.groups
-  end
-
-  def new
-    @group = Group.new
-    @group.users << current_user
-  end
-
-  def create
-    @group = Group.new(group_params)
-    if @group.save
-      redirect_to root_path, notice: "作成に成功しました。"
-    else
-      redirect_to new_group_path, alert: "作成に失敗しました。"
+    def new
+      @group = Group.new
+      @group.users << current_user
     end
-  end
 
-  def edit
-  end
-
-  def update
-    if @group.update(group_params)
-      redirect_to root_path, notice: "更新に成功しました。"
-    else
-      redirect_to edit_group_path, alert: "更新に失敗しました。"
+    def create
+      @group = Group.new(group_params)
+      if @group.save
+        GroupUser.create(user_id: current_user.id, group_id: @group.id)
+        redirect_to group_path(@group), notice: 'グループが作成されました'
+      else
+        flash[:alert] = 'グループに失敗しました'
+        render :new
+      end
     end
-  end
+
+    def edit
+    end
+
+    def show
+      @groups = current_user.groups
+    end
+
+    def update
+      if @group.update(group_params)
+        redirect_to group_path(@group), notice: '更新に成功しました'
+      else
+        flash[:alert] = '更新に失敗しました'
+        render :edit
+      end
+    end
 
   private
-  def set_group
-    @group = Group.find_by(params[:id])
-  end
+    def set_group
+      @group = Group.find(params[:id])
+    end
 
-  def group_params
-    params.require(:group).permit(:name, { user_ids: [] })
+    def group_params
+      params.require(:group).permit(:name, { user_ids: [] })
+    end
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :groups, except: [:show, :destroy] do
-    resources :messages, only: [:index]
-  end
+  resources :groups
+  resources :messages
 end
